7241f786b476bc457240e118cf90cbdc66cc12b2
Christian Netto <christiannettom@gmail.com>
2026-05-03 19:31:45 -0300

    feat: backend inicial com autenticação JWT

---
+++ backend-app/chrisfinance/src/main/java/com/chrisdev/chrisfinance/ChrisfinanceApplication.java
@@ -0,0 +1,13 @@
+package com.chrisdev.chrisfinance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrisfinanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrisfinanceApplication.class, args);
+	}
+
+}
